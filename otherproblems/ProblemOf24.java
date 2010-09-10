cb46bb04df99dd7462de05916ab6cada36247619
jfresen <jfresen@3a2a3df4-8c49-f40c-08c3-bb1c6002b544>
2010-09-10 15:03:07 +0000

    Problem of making 24 out of the numbers 1, 3, 4 and 6
    git-svn-id: https://icpc.googlecode.com/svn/icpc@30 3a2a3df4-8c49-f40c-08c3-bb1c6002b544

---
+++ otherproblems/ProblemOf24.java
@@ -0,0 +1,52 @@
+package otherproblems;
+
+public class ProblemOf24
+{
+	public static double EPS = 0.01;
+	public static String ans = "(#)";
+	
+	public static void main(String[] args)
+	{
+		int[] number = {1, 3, 4, 6};
+		int c = number[0];
+		number = remove(number, 0);
+		if (solve(number, c) && store(""+c))
+			System.out.println("Problem solved, solution is: "+ans);
+		else
+			System.out.println("Problem could not be solved");
+	}
+
+	private static boolean solve(int[] number, double c)
+	{
+		if (number.length == 0)
+			return 24-EPS < c && c < 24+EPS;
+		
+		for (int i = 0; i < number.length; i++)
+		{
+			int x = number[i];
+			int[] remaining = remove(number, i);
+			if (solve(remaining, x+c)) return store(x+"+(#)");
+			if (solve(remaining, x-c)) return store(x+"-(#)");
+			if (solve(remaining, c-x)) return store("(#)-"+x);
+			if (solve(remaining, c*x)) return store("(#)*"+x);
+			if (solve(remaining, c/x)) return store("(#)/"+x);
+			if (solve(remaining, x/c)) return store(x+"/(#)");
+		}
+		
+		return false;
+	}
+
+	private static boolean store(String expr)
+	{
+		ans = ans.replace("#", expr);
+		return true;
+	}
+
+	private static int[] remove(int[] number, int i)
+	{
+		int[] remaining = new int[number.length-1];
+		System.arraycopy(number, 0, remaining, 0, i);
+		System.arraycopy(number, i+1, remaining, i, remaining.length-i);
+		return remaining;
+	}
+}
