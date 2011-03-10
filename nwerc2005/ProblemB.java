96a8d27716b90f9b1ca646bf3c5fe49603c777cf
jellefresen <jellefresen@3a2a3df4-8c49-f40c-08c3-bb1c6002b544>
2011-03-10 23:00:57 +0000

    git-svn-id: https://icpc.googlecode.com/svn/icpc@47 3a2a3df4-8c49-f40c-08c3-bb1c6002b544

--- nwerc2005/ProblemB.java
+++ nwerc2005/ProblemB.java
@@ -1,229 +1,119 @@
 package nwerc2005;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Hashtable;
-import java.util.StringTokenizer;
+import java.io.File;
+import java.util.Scanner;
 
 public class ProblemB
 {
-
-	static Hashtable<String, Integer> ingrToNum;
-	static Hashtable<Integer, String> numToProd;
 	
 	static int P, Q;
-	static int maxFromLastFixed, lastFixed;
-	static int sumMin = 0;
+	static Product[] product;
 	
-	static Ingredient[][] ingrCalc;
-	static Ingredient[][] ingr;
-	static Ingredient[] solutions = new Ingredient[10];
-	
-	static BufferedReader in;
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception
+	public static void main(String[] args) throws Throwable
 	{
-		ingrToNum = new Hashtable<String, Integer>();
-		numToProd = new Hashtable<Integer, String>();
-		in = new BufferedReader(new FileReader("ekp2005\\sampledata\\declaration.in"));
-//		in = new BufferedReader(new InputStreamReader(System.in));
-		P = Integer.parseInt(in.readLine());
+		Scanner in = new Scanner(new File("nwerc2005/testdata/b.in"));
+		while ((P=in.nextInt()) != 0)
+		{
+			product = new Product[P];
+			for (int i = 0; i < P; i++)
+				product[i] = new Product(in);
+			Q = in.nextInt();
+			for (int i = 0; i < Q; i++)
+				executeQuery(in);
+		}
+	}
+	
+	private static void executeQuery(Scanner in)
+	{
+		String type = in.next();
+		String ingredient = in.next();
+		int l = 0, u = 100;
+		for (int i = 0, j; i < P; i++)
+		{
+			j = product[i].find(ingredient);
+			l = Math.max(l, j != -1 ? product[i].lb[j] : 0);
+			u = Math.min(u, j != -1 ? product[i].ub[j] : 0);
+		}
+		String ans = "";
+		if (type.equals("most"))
+			for (int i = 0, j; i < P; i++)
+				if (((j=product[i].find(ingredient)) != -1 ? product[i].ub[j] : 0) >= l)
+					ans += product[i].name+" ";
+		if (type.equals("least"))
+			for (int i = 0, j; i < P; i++)
+				if (((j=product[i].find(ingredient)) != -1 ? product[i].lb[j] : 0) <= u)
+					ans += product[i].name+" ";
+		System.out.println(ans.substring(0, ans.length()-1));
+	}
+	
+	private static class Product
+	{
+		int n;
+		String name;
+		String[] ingredient;
+		int[] lb, ub;
 		
-		while (P != 0)
+		public Product(Scanner in)
 		{
-			readTest();
-			processInput();
-			executeQueries();
-			P = Integer.parseInt(in.readLine());
+			name = in.next();
+			n = in.nextInt();
+			lb = new int[n];
+			ub = new int[n];
+			ingredient = new String[n];
+			for (int i = n-1; i >= 0; i--)
+			{ // (store ingredients in increasing order)
+				ingredient[i] = in.next();
+				String s = in.nextLine();
+				if ("".equals(s))
+					lb[i] = (ub[i] = 100) - 99;
+				else
+					lb[i] = ub[i] = Integer.parseInt(s.substring(1, s.length()-1));
+			}
+			tightenBounds();
 		}
 		
-	}
-	
-	public static void readTest() throws IOException
-	{
-		ingrCalc = new Ingredient[P+1][];
-		ingr = new Ingredient[P+1][1001];
-		for (int productI = 1; productI <= P; productI++)
+		private void tightenBounds()
 		{
-			String prodName = in.readLine();
-			numToProd.put(productI, prodName);
-			int n = Integer.parseInt(in.readLine());
-			ingrCalc[productI] = new Ingredient[n+1];
-			for (int ingredientI = 1; ingredientI <= n; ingredientI++)
+			for (int i = 0, a = 0; i < n; i++)
+				a = lb[i] = Math.max(lb[i], a);
+			for (int i = n-1, a = 100; i >= 0; i--)
+				a = ub[i] = Math.min(ub[i], a);
+			boolean changed = true;
+			while (changed)
 			{
-				StringTokenizer input = new StringTokenizer(in.readLine());
-				String ingrName = input.nextToken();
-				Ingredient ing = null;
-				Integer ingrNum = ingrToNum.get(ingrName);
-				if (ingrNum == null)
+				changed = false;
+				for (int i = 0; i < n; i++)
 				{
-					ing = new Ingredient(productI, ingrToNum.size());
-					ingrToNum.put(ingrName, ing.ingr);
+					int l = 0, u = 0, j; // lower, upper, j
+					for (j = i+1; j < n; j++)                      u += ub[j];
+					for (j =   0; j < i && ub[j] < lb(u,i,j); j++) u += ub[j];
+					if (lb[i] < lb(u,i,j))
+						changed = (lb[i] = lb(u,i,j)) == lb[i];
+					for (j =   0; j < i; j++)                      l += lb[j];
+					for (j = n-1; j > i && lb[j] > ub(l,i,j); j--) l += lb[j];
+					if (ub[i] > ub(l,i,j))
+						changed = (ub[i] = ub(l,i,j)) == ub[i];
 				}
-				else
-				{
-					ing = new Ingredient(productI, ingrNum);
-				}
-				if (input.hasMoreTokens())
-				{
-					String percentage = input.nextToken();
-					ing.min = ing.max = Integer.parseInt(percentage.substring(0,percentage.length()-1));
-				}
-				ingr[productI][ing.ingr] = ing;
-				ingrCalc[productI][ingredientI] = ing;
 			}
+		}
+		
+		private int lb(int upper, int i, int j)
+		{
+			return (int)Math.ceil((100d-upper)/(i-j+1));
+		}
+		
+		private int ub(int lower, int i, int j)
+		{
+			return (int)Math.floor((100d-lower)/(j-i+1));
+		}
+		
+		public int find(String needle)
+		{
+			for (int i = 0; i < n; i++)
+				if (ingredient[i].equals(needle))
+					return i;
+			return -1;
 		}
 	}
 	
-	public static void processInput()
-	{
-		for (int i = 1; i <= P; i++)
-		{
-			calc(i);
-		}
-	}
-	
-	public static void calc(int px)
-	{
-		maxFromLastFixed = 0;
-		sumMin = 0;
-		Ingredient[] ingrInProduct = ingrCalc[px];
-		int n = ingrInProduct.length - 1;
-		lastFixed = n+1;
-		for (int i = n; i > 0; i--)
-		{
-			if (ingrInProduct[i].min != 0)
-			{
-				int min = ingrInProduct[i].min;
-				int sum = 0;
-				for (int j = lastFixed - 1; j >= i; j--)
-				{
-					ingrInProduct[j].max = min;
-					sum += min;
-				}
-				maxFromLastFixed += sum;
-				lastFixed = i;
-			}
-			else
-			{
-				if (i == n)
-				{
-					ingrInProduct[i].min = 1;
-				}
-				else if (i == 1)
-				{
-					double min = (100.0 - maxFromLastFixed) / (lastFixed - 1);
-					if (min - Math.floor(min) != 0.0)
-						min++;
-					ingrInProduct[i].min = (int)Math.floor(min);
-				}
-				else
-				{
-					ingrInProduct[i].min = ingrInProduct[i+1].min;
-				}
-				ingrInProduct[i].max = (int)((100.0 - sumMin) / i);
-			}
-			sumMin += ingrInProduct[i].min;
-		}
-	}
-	
-	public static void executeQueries() throws IOException
-	{
-		Q = Integer.parseInt(in.readLine());
-		for (int query = 0; query < Q; query++)
-		{
-			StringTokenizer input = new StringTokenizer(in.readLine());
-			String leastMost = input.nextToken();
-			String ingrName = input.nextToken();
-			Integer ingrNumObject = ingrToNum.get(ingrName);
-			if (ingrNumObject == null)
-			{
-				for (int i = 1; i <= P; i++)
-				{
-					System.out.print(numToProd.get(i) + " ");
-				}
-				System.out.println();
-				break;
-			}
-			int ingrNum = ingrNumObject;
-			int numSolutions = 0;
-			for (int i = 1; i <= P; i++)
-			{
-				if (ingr[i][ingrNum] != null)
-				{
-					solutions[numSolutions] = ingr[i][ingrNum];
-					numSolutions++;
-				}
-			}
-			if (leastMost.equals("least"))
-			{
-				for (int i = 0; i < numSolutions; i++)
-				{
-					for (int j = i+1; !solutions[i].dropped && j < numSolutions; j++)
-					{
-						solutions[i].filterMin(solutions[j]);
-					}
-				}
-			}
-			else // most
-			{
-				for (int i = 0; i < numSolutions; i++)
-				{
-					for (int j = i+1; !solutions[i].dropped && j < numSolutions; j++)
-					{
-						solutions[i].filterMax(solutions[j]);
-					}
-				}
-			}
-			for (int i = 0; i < numSolutions; i++)
-			{
-				if (!solutions[i].dropped)
-				{
-					System.out.print(numToProd.get(solutions[i].prod) + " ");
-				}
-			}
-			System.out.println();
-		}
-	}
-
-}
-
-class Ingredient
-{
-	
-	public Ingredient(int prod, int ingr)
-	{
-		this.prod = prod;
-		this.ingr = ingr;
-	}
-	
-	int min = 0, max = 0, prod, ingr;
-	boolean dropped = false;
-	
-	public void filterMin(Ingredient that)
-	{
-		if (this.max < that.min)
-			that.dropped = true;
-		else if (this.min > that.max)
-			this.dropped = true;
-	}
-	
-	public void filterMax(Ingredient that)
-	{
-		if (this.max < that.min)
-			this.dropped = true;
-		else if (this.min > that.max)
-			that.dropped = true;
-	}
-	
-	@Override
-	public String toString()
-	{
-		return ingr + ": " + min + " " + max;
-	}
-
 }
