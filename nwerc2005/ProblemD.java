a32f18cf1220b3fafa6dcd63f7b8907ad1f30ec9
jellefresen <jellefresen@3a2a3df4-8c49-f40c-08c3-bb1c6002b544>
2011-03-23 17:54:30 +0000

    git-svn-id: https://icpc.googlecode.com/svn/icpc@49 3a2a3df4-8c49-f40c-08c3-bb1c6002b544

--- nwerc2005/ProblemD.java
+++ nwerc2005/ProblemD.java
@@ -1,44 +1,88 @@
 package nwerc2005;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.File;
+import java.util.Scanner;
 
-public class ProblemD {
-
-	static int[] hs, rs, Rs;
-	static double[][] heights;
-	static int bowls;
+public class ProblemD
+{
 	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws Exception {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(in.readLine());
-		
-		for (int i = 0; i < n; i++) {
-			bowls = Integer.parseInt(in.readLine());
-			hs = new int[bowls];
-			rs = new int[bowls];
-			Rs = new int[bowls];
-			
-			for (int j = 0; j < bowls; j++) {
-				String[] token = in.readLine().split(" ");
-				int h = Integer.parseInt(token[0]);
-				int r = Integer.parseInt(token[1]);
-				int R = Integer.parseInt(token[2]);
-				
-				hs[j] = h;
-				rs[j] = r;
-				Rs[j] = R;
+	static int n;
+	static int[] h, r, R;  // height, lower radius, upper radius
+	static double[] b;     // bottom
+	static boolean[] used;
+	
+	public static void main(String[] args) throws Throwable
+	{
+		Scanner in = new Scanner(new File("nwerc2005/testdata/d.in"));
+		int cases = in.nextInt();
+		while (cases-- > 0)
+		{
+			n = in.nextInt();
+			h = new int[n];
+			r = new int[n];
+			R = new int[n];
+			b = new double[n];
+			used = new boolean[n];
+			for (int i = 0; i < n; i++)
+			{
+				h[i] = in.nextInt();
+				r[i] = in.nextInt();
+				R[i] = in.nextInt();
 			}
-		}
-		
-		for (int x = 0; x < bowls; x++) {
-			for (int y = 0; y < bowls; y++) {
-				
-			}
+			System.out.println((int)min(0));
 		}
 	}
-
+	
+	private static double min(int i)
+	{
+		if (i == n)
+			return height();
+		double minheight = Double.MAX_VALUE;
+		for (int j = 0; j < n; j++)
+			if (!used[j])
+			{
+				b[j] = bottom(j);
+				used[j] = true;
+				minheight = Math.min(minheight, min(i+1));
+				used[j] = false;
+			}
+		return minheight;
+	}
+	
+	private static double height()
+	{
+		// Given b[j], j=0..n-1, calculate the height of the stack.
+		double height = b[0] + h[0];
+		for (int i = 1; i < n; i++)
+			if (height < b[i] + h[i])
+				height = b[i] + h[i];
+		return height;
+	}
+	
+	private static double bottom(int i)
+	{
+		double bottom = 0;
+		for (int j = 0; j < n; j++)
+			if (used[j] && bottom < bottom(j, i))
+				bottom = bottom(j, i);
+		return bottom;
+	}
+	
+	private static double bottom(int l, int u)
+	{
+		double ml = (double)h[l]/(R[l]-r[l]);
+		double mu = (double)h[u]/(R[u]-r[u]);
+		// upper stands on top of lower
+		if (r[u] >= R[l])
+			return b[l] + h[l];
+		// upper hangs in brim of lower
+		if (ml >= mu && R[u] >= R[l])
+			return Math.max(b[l], b[l] + h[l] - mu*(R[l]-r[u]));
+		// upper hangs against the side of lower if lower is deep enough
+		if (ml >= mu)
+			return Math.max(b[l], b[l] - h[u] + ml*(R[u]-r[l]));
+		// upper stands against the side of lower if lower is deep enough
+		return Math.max(b[l], b[l] + ml*(r[u]-r[l]));
+	}
+	
 }
